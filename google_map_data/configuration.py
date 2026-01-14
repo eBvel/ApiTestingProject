e12817f0b8443f9bc7842885c559e8584570796f
eBvel <belyakov041098@gmail.com>
2026-01-14 16:36:02 +0700

    Created configuration file that contains end points of google map API.

---
+++ google_map_data/configuration.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class GoogleMapEndPoints:
+    base_url = "https://rahulshettyacademy.com"
+    key_param: str = "?key=qaclick123"
+    url_of_create_location : str = \
+        f"{base_url}/maps/api/place/add/json{key_param}"
+    url_of_edit_location : str = \
+        f"{base_url}/maps/api/place/update/json{key_param}"
+    url_of_get_location : str = \
+        f"{base_url}/maps/api/place/get/json{key_param}"
+    url_of_delete_location : str = \
+        f"{base_url}/maps/api/place/delete/json{key_param}"
