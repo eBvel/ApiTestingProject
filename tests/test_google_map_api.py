f2667a516561e0fae267f0bf61cd51ad3450c812
eBvel <belyakov041098@gmail.com>
2026-01-17 14:11:10 +0700

    added a check of get_location method to the test.

--- tests/test_google_map_api.py
+++ tests/test_google_map_api.py
@@ -2,11 +2,18 @@ from requests import Response
 from utils.api import GoogleMapAPI as api
 
 
+def compare_values(value_name, current_value, expected_value):
+    print(f"\n{value_name}\n{current_value=}\n{expected_value=}")
+    assert current_value == expected_value, f"Incorrect value {current_value}"
+    print(f"PASSED: Value '{current_value}' is correct!")
+
+
 class TestGoogleMapAPI:
     def test_create_location(self, default_body):
-        print("TEST: Create location (POST)")
-        response : Response = api.create_location(default_body)
-        response_status_code = response.status_code
-        print(f"Status-code: {response_status_code}")
-        assert response_status_code == 200
-        print(f"PASSED: Status code is correct!")
+        print("Create location (POST)")
+        post_response : Response = api.create_location(default_body)
+        compare_values("POST status-code", post_response.status_code, 200)
+
+        print("Get location (GET)")
+        get_response : Response = api.get_location(post_response.json().get('place_id'))
+        compare_values("GET status-code", get_response.status_code, 200)
