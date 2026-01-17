f962920cbe85452401974d75bf0a70b2bc8bae53
eBvel <belyakov041098@gmail.com>
2026-01-17 16:49:13 +0700

    created make_body_for_update_location fixture that contains a new location address.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,14 @@ def default_body():
     return JsonManager.read(
         abspath("..") + "\\google_map_data\\default_location_body.json"
     )
+
+
+@pytest.fixture
+def make_body_for_update_location():
+    def _make_body_for_update_location(place_id):
+        return {
+            "place_id": place_id,
+            "address": "100 Lenina street, RU",
+            "key": "qaclick123"
+        }
+    return _make_body_for_update_location
