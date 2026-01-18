54cc9571c882b56ee6f8a1c018ba614b4fab52bb
eBvel <belyakov041098@gmail.com>
2026-01-18 17:09:14 +0700

    Added delete_location method. Adapted class for work with session.

--- utils/api.py
+++ utils/api.py
@@ -3,26 +3,34 @@ from google_map_data.configuration import GoogleMapEndPoints as end_points
 
 
 class GoogleMapAPI:
-    @staticmethod
-    def create_location(body):
+    def __init__(self, session):
+        self.session = session
+        self.custom_requests = CustomRequests(session)
+
+    def create_location(self, body):
         url = end_points.url_of_create_location
         print(url)
-        response = CustomRequests.post(url, body)
+        response = self.custom_requests.post(url, body)
         print(response.text)
         return response
 
-    @staticmethod
-    def get_location(place_id):
+    def get_location(self, place_id):
         url = end_points.url_of_get_location+place_id
         print(url)
-        response = CustomRequests.get(url)
+        response = self.custom_requests.get(url)
         print(response.text)
         return response
 
-    @staticmethod
-    def update_location(body):
+    def update_location(self, body):
         url = end_points.url_of_edit_location
         print(url)
-        response = CustomRequests.put(url, body)
+        response = self.custom_requests.put(url, body)
+        print(response.text)
+        return response
+
+    def delete_location(self, body):
+        url = end_points.url_of_delete_location
+        print(url)
+        response = self.custom_requests.delete(url, body)
         print(response.text)
         return response
