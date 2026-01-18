9d77838b14ee9cc9f6f259ca51a81b5bc39092d6
eBvel <belyakov041098@gmail.com>
2026-01-18 17:11:46 +0700

    Added session_connection fixture that create once instance Session for class.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,17 @@
 import pytest
+from utils.api import GoogleMapAPI
 from utils.json_manager import JsonManager
 from os.path import abspath
+from requests import Session
+
+
+@pytest.fixture(scope="class")
+def session_connection(request):
+    print("OPEN SESSION")
+    with Session() as session:
+        request.cls.api = GoogleMapAPI(session)
+        yield
+    print("CLOSE SESSION")
 
 
 @pytest.fixture
@@ -12,10 +23,17 @@ def default_body():
 
 @pytest.fixture
 def make_body_for_update_location():
-    def _make_body_for_update_location(place_id):
+    def _body_for_update_location(place_id):
         return {
             "place_id": place_id,
             "address": "100 Lenina street, RU",
             "key": "qaclick123"
         }
-    return _make_body_for_update_location
+    return _body_for_update_location
+
+
+@pytest.fixture
+def make_body_for_delete_location():
+    def _body_for_delete_location(place_id):
+        return {"place_id": place_id}
+    return _body_for_delete_location
