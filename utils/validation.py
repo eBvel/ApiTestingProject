60f5221f556ee9c19ac4497d6ac2d40bfe7c4d00
eBvel <belyakov041098@gmail.com>
2026-01-19 15:04:04 +0700

    Created compare_response_token method.

--- utils/validation.py
+++ utils/validation.py
@@ -1,8 +1,17 @@
+import json
+from requests import  Response
 
 
 class ValueValidation:
     @staticmethod
     def compare_values(value_name, current_value, expected_value):
         print(f"\n{value_name}\n{current_value=}\n{expected_value=}")
-        assert current_value == expected_value, f"Incorrect value {current_value}"
+        assert current_value == expected_value, (f"Incorrect value "
+                                                 f"{current_value}")
         print(f"PASSED: Value '{current_value}' is correct!")
+
+    @staticmethod
+    def compare_response_token(response: Response, expected_token):
+        response_token = json.loads(response.text)
+        assert list(response_token) == expected_token
+        print(f"PASSED: Token is correct! All fields are included.")
