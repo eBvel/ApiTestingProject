976114579b273ab16909485e3b4ed1344a0f26a7
eBvel <belyakov041098@gmail.com>
2026-01-19 13:22:56 +0700

    Added status-code validation.

--- tests/test_google_map_api.py
+++ tests/test_google_map_api.py
@@ -1,11 +1,6 @@
 import pytest
 from requests import Response
-
-
-def compare_values(value_name, current_value, expected_value):
-    print(f"\n{value_name}\n{current_value=}\n{expected_value=}")
-    assert current_value == expected_value, f"Incorrect value {current_value}"
-    print(f"PASSED: Value '{current_value}' is correct!")
+from utils.validation import ValueValidation
 
 
 @pytest.mark.usefixtures("session_connection")
@@ -13,24 +8,44 @@ class TestGoogleMapAPI:
     def test_create_location(self, default_body):
         print("Create location (POST)")
         post_response : Response = self.api.create_location(default_body)
-        compare_values("POST status-code", post_response.status_code, 200)
+        ValueValidation.compare_values(
+            "POST status-code",
+            post_response.status_code,
+            200
+        )
 
     def test_get_location(self, default_body):
         print("Create location (POST)")
         post_response: Response = self.api.create_location(default_body)
 
         print("Get location (GET-POST)")
-        get_response: Response = self.api.get_location(post_response.json().get('place_id'))
-        compare_values("GET status-code", get_response.status_code, 200)
+        get_response: Response = self.api.get_location(
+            post_response.json().get('place_id')
+        )
+        ValueValidation.compare_values(
+            "GET status-code",
+            get_response.status_code,
+            200
+        )
 
-    def test_update_location(self, default_body, make_body_for_update_location):
+    def test_update_location(
+            self,
+            default_body,
+            make_body_for_update_location
+    ):
         print("Create location (POST)")
         post_response: Response = self.api.create_location(default_body)
 
         print("Update location (POST-PUT)")
-        body = make_body_for_update_location(post_response.json().get('place_id'))
+        body = make_body_for_update_location(
+            post_response.json().get('place_id')
+        )
         put_response: Response = self.api.update_location(body)
-        compare_values("PUT status-code", put_response.status_code, 200)
+        ValueValidation.compare_values(
+            "PUT status-code",
+            put_response.status_code,
+            200
+        )
 
     def test_delete_location(
             self,
@@ -44,11 +59,20 @@ class TestGoogleMapAPI:
         print("Delete location (POST-DELETE)")
         body = make_body_for_delete_location(place_id)
         delete_response : Response = self.api.delete_location(body)
-        compare_values("DELETE status-code", delete_response.status_code, 200)
+        ValueValidation.compare_values(
+            "DELETE status-code",
+            delete_response.status_code,
+            200
+        )
 
         print("Get location (POST-DELETE-GET)")
         get_response : Response = self.api.get_location(place_id)
-        compare_values(
+        ValueValidation.compare_values(
+            "GET status-code",
+            get_response.status_code,
+            404
+        )
+        ValueValidation.compare_values(
             "GET msg",
             get_response.json().get('msg'),
             "Get operation failed, looks like place_id  doesn't exists"
